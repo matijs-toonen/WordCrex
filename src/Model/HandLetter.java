0c4cdeb750074ef17c506a12eb52fab86fb1704b
Matijs <matijstoonen@gmail.com>
2018-12-02 13:16:44 +0100

    added db model handletter

--- src/Model/HandLetter.java
+++ src/Model/HandLetter.java
@@ -1,5 +1,9 @@
 package Model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class HandLetter {
 	private Game _game;
 	private Turn _turn;
@@ -9,5 +13,16 @@ public class HandLetter {
 		_game = game;
 		_turn = turn;
 		_letter = letter;
+	}
+	
+	public HandLetter(ResultSet rs, ArrayList<String> columns) {
+		try {
+			_game = columns.contains("game_id") ? new Game(rs.getInt("game_id")) : null;
+			_turn = columns.contains("turn_id") ? new Turn(rs.getInt("turn_id")) : null;
+			_letter = columns.contains("letter_id") ? new Letter(rs.getInt("letter_id")) : null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
