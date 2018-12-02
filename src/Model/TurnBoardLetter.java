81cbd45edfa825b5b18ebb34a7a0ba4aaf73750b
Matijs <matijstoonen@gmail.com>
2018-12-02 14:53:52 +0100

    added db model for turnBoardLetter

--- src/Model/TurnBoardLetter.java
+++ src/Model/TurnBoardLetter.java
@@ -1,5 +1,9 @@
 package Model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class TurnBoardLetter {
 	private Letter _letter;
 	private Game _game;
@@ -11,5 +15,17 @@ public class TurnBoardLetter {
 		_game = game;
 		_turn = turn;
 		_tile = tile;
+	}
+	
+	public TurnBoardLetter(ResultSet rs, ArrayList<String> columns) {
+		try {
+			_letter = columns.contains("letter_id") ? new Letter(rs.getInt("letter_id")) : null;
+			_game = columns.contains("game_id") ? new Game(rs.getInt("game_id")) : null;
+			_turn = columns.contains("turn_id") ? new Turn(rs.getInt("turn_id")) : null;
+			_tile = columns.contains("tile_x") && columns.contains("tile_y") ? new Tile(rs.getInt("tile_x"), rs.getInt("tile_y")) : null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
