b0d94829452fb8ad7b5d201c946023debed24b15
Matijs <matijstoonen@gmail.com>
2018-12-02 14:51:34 +0100

    added db model for turn

--- src/Model/Turn.java
+++ src/Model/Turn.java
@@ -1,7 +1,11 @@
 package Model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Turn {
-	private int _turnId;
+	private Integer _turnId;
 	private Game _game;
 	
 	public Turn(int turnId) {
@@ -11,5 +15,15 @@ public class Turn {
 	public Turn(int turnId, Game game) {
 		this(turnId);
 		_game = game;
+	}
+	
+	public Turn(ResultSet rs, ArrayList<String> columns) {
+		try {
+			_turnId = columns.contains("turn_id") ? rs.getInt("turn_id") : null;
+			_game = columns.contains("game_id") ? new Game(rs.getInt("game_id")) : null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
