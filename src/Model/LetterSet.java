be180ad82a9201d49ac5533501ba97ae64d27623
Matijs <matijstoonen@gmail.com>
2018-12-02 13:25:59 +0100

    added db model for letterset

--- src/Model/LetterSet.java
+++ src/Model/LetterSet.java
@@ -1,5 +1,9 @@
 package Model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class LetterSet {
 	private String _letterCode;
 	private String _description;
@@ -11,6 +15,16 @@ public class LetterSet {
 	public LetterSet(String letterCode, String description) {
 		this(letterCode);
 		_description = description;
+	}
+	
+	public LetterSet(ResultSet rs, ArrayList<String> columns) {
+		try {
+			_letterCode = columns.contains("code") ? rs.getString("code") : null;
+			_description = columns.contains("description") ? rs.getString("description") : null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public String getLetterCode() {
