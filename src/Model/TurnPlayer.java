f91f47b35c4ccb0b6512bc3594d94f86ffedc3ed
Matijs <matijstoonen@gmail.com>
2018-12-02 15:05:14 +0100

    added db model for turnPlayer

--- src/Model/TurnPlayer.java
+++ src/Model/TurnPlayer.java
@@ -1,13 +1,16 @@
 package Model;
 
-import Model.TurnAction.TurnAction;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import Model.TurnAction.*;
 
 public class TurnPlayer {
 	private Game _game;
 	private Turn _turn;
 	private Account _user;
-	private int _bonus;
-	private int _score;
+	private Integer _bonus;
+	private Integer _score;
 	private TurnAction _turnAction;
 	
 	public TurnPlayer(Game game, Turn turn, Account user, int bonus, int score, TurnAction turnAction) {
@@ -17,5 +20,42 @@ public class TurnPlayer {
 		_bonus = bonus;
 		_score = score;
 		_turnAction = turnAction;
+	}
+	
+	public TurnPlayer(ResultSet rs, ArrayList<String> columns) {
+		try {
+			_game = columns.contains("game_id") ? new Game(rs.getInt("game_id")) : null;
+			_turn = columns.contains("turn_id") ? new Turn(rs.getInt("turn_id")) : null;
+			var playerNumber = checkPlayerNumber(columns);
+			_user = playerNumber != null ? new Account(rs.getString("username_player" + playerNumber)) : null;
+			_bonus = columns.contains("bonus") ? rs.getInt("bonus") : null;
+			_score = columns.contains("score") ? rs.getInt("score") : null;
+			_turnAction = columns.contains("turnaction_type") ? getTurnAction(rs.getString("turnaction_type")) : null;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private Integer checkPlayerNumber(ArrayList<String> columns) {
+		if(columns.contains("username_player1"))
+			return 1;
+		else if(columns.contains("username_player2"))
+			return 2;
+		else
+			return null;
+	}
+	
+	private TurnAction getTurnAction(String turnAction) {
+		switch(turnAction.toLowerCase()) {
+		case "play":
+			return new PlayTurnAction();
+		case "pass":
+			return new PassTurnAction();
+		case "resign":
+			return new ResignTurnAction();
+		default:
+			return null;
+		}
 	}
 }
