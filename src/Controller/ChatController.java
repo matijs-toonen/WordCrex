1fbfb5b33f649280fc200d09c1ce65f77b545158
Ali Bader <bader@digitalbyali.nl>
2018-12-05 20:27:16 +0100

    chat elements and actions

--- src/Controller/ChatController.java
+++ src/Controller/ChatController.java
@@ -1,33 +1,45 @@
 package Controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ChatController 
-{
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class ChatController implements Initializable {
 	//dummy values
 	private String player = "Davod";
 	private String time = "5-12-2018 16:46";
 	
+	@FXML
+	private VBox textScreen;
 	
 	@FXML
-	private TextArea textScreen;
+	private TextField chatInput;
 	
-	@FXML
-	private TextArea chatInput;
-	
-	public void sendMessage(KeyEvent e)
-	{
-		if(e.getCode() == KeyCode.ENTER)
-		{	
-			String inputMessage = chatInput.getText();
-			String finalMessage =  "(" + time + ") " + "<" + player + ">: " + inputMessage;
-			
-			textScreen.setText(textScreen.getText() + finalMessage + "\n");
-			textScreen.appendText("");
-			chatInput.setText("");
-		}
+	public void submitChatInput(ActionEvent e) {
+		sendMessage();
+	}
+
+	private void sendMessage() {
+		
+		Label label = new Label();
+		label.setText("Bader");
+		
+		textScreen.getChildren().add(label);
+		
+		System.out.println("Sending Message");
+		
+		chatInput.setText("");
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
 	}
 }
