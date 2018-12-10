e7208ca6ff0b3ec774e9a9bf321a6e71e1e35ecf
Xandor Janssen <xpjh.janssen@student.avans.nl>
2018-12-10 14:46:46 +0100

    history query done

--- queries/queries.sql
+++ queries/queries.sql
@@ -81,7 +81,7 @@ where (g.username_player1 = @username
 GROUP BY g.game_id;
 
 
--- gebruikers beheren
+-- gebruikers beherenqqqqqqqqqqqqqq
 
 SELECT 
     *
@@ -94,4 +94,83 @@ SET @role = 'player';
 
 
 INSERT INTO accountrole values(@username,@role);
+
+
+-- instellingen 
+SET @username = 'jagermeester';
+
+
+SELECT 
+    username, role
+FROM
+    accountrole
+WHERE
+    username = @username;
+    
+    
+    
+SET @usernamee = 'jagermeester';    
+SET @wachtwoord = 'P@ssw0rd'; 
+
+
+UPDATE `account`
+SET `password` = @wachtwoord
+WHERE username = @usernamee;
+
+
+
+-- history bader
+
+select * from gelegdplayer1;
+
+select * from gelegdplayer2;
+
+select * from gelegd;
+
+
+select tp1.game_id,tp1.turn_id, tp1.username_player1, tp1.score+tp1.bonus as score , gp1.woorddeel 
+from turnplayer1 as tp1
+inner join gelegdplayer1 as gp1
+on tp1.game_id = gp1.game_id
+and 
+tp1.turn_id = gp1.turn_id;
+
+
+select tp2.game_id,tp2.turn_id, tp2.username_player2, tp2.score+tp2.bonus as score, gp2.woorddeel 
+from turnplayer2 as tp2
+inner join gelegdplayer2 as gp2
+on tp2.game_id = gp2.game_id
+and 
+tp2.turn_id = gp2.turn_id;
+
+
+-- -- -- -- -- -- -- -- -- -- -- -- 
+
+SELECT 
+    tp1.game_id,
+    tp1.turn_id,
+    tp1.username_player1,
+    tp1.score + tp1.bonus AS score,
+    gp1.woorddeel,
+    tp2.username_player2,
+    tp2.score + tp2.bonus AS score,
+    gp2.woorddeel
+FROM
+    turnplayer1 AS tp1
+        INNER JOIN
+    gelegdplayer1 AS gp1 ON tp1.game_id = gp1.game_id
+        AND tp1.turn_id = gp1.turn_id
+        INNER JOIN
+    turnplayer2 AS tp2 ON tp1.game_id = tp2.game_id
+        AND tp1.turn_id = tp2.turn_id
+        INNER JOIN
+    gelegdplayer2 AS gp2 ON tp2.game_id = gp2.game_id
+        AND tp2.turn_id = gp2.turn_id;
+;
+
+select * from turn;
+
+select * from turnplayer1;
+
+select * from turnplayer2;
 
