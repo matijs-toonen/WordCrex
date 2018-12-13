ad75942b582c1aedfc13e2c58defd251b96e81e1
Xandor Janssen <xpjh.janssen@student.avans.nl>
2018-12-13 11:16:18 +0100

    no message

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,7 +1,6 @@
 -- Games
 -- klaar en winnaar
 
-
 SET @username = 'jagermeester';
 
 SELECT 
@@ -101,6 +100,7 @@ INSERT INTO accountrole values(@username,@role);
 
 
 -- instellingen 
+--
 SET @username = 'jagermeester';
 
 
@@ -110,7 +110,7 @@ FROM
     accountrole
 WHERE
     username = @username;
-    
+--
     
     
 SET @usernamee = 'jagermeester';    
@@ -124,24 +124,6 @@ WHERE username = @usernamee;
 
 
 -- history bader
-
-
-
-select tp1.game_id,tp1.turn_id, tp1.username_player1, tp1.score+tp1.bonus as score , gp1.woorddeel 
-from turnplayer1 as tp1
-inner join gelegdplayer1 as gp1
-on tp1.game_id = gp1.game_id
-and 
-tp1.turn_id = gp1.turn_id;
-
-
-select tp2.game_id,tp2.turn_id, tp2.username_player2, tp2.score+tp2.bonus as score, gp2.woorddeel 
-from turnplayer2 as tp2
-inner join gelegdplayer2 as gp2
-on tp2.game_id = gp2.game_id
-and 
-tp2.turn_id = gp2.turn_id;
-
 
 -- -- -- -- -- -- -- -- -- -- -- -- 
 SELECT 
@@ -168,4 +150,84 @@ WHERE
     tp1.game_id = 502;
 
 
+-- BORD
+
+-- hand has letter + score (matijs)
+SELECT * 
+FROM handletter 
+NATURAL JOIN letter 
+NATURAL JOIN symbol 
+where game_id =  500
+AND turn_id = (select max(turn_id)
+from handletter
+where game_id = 500);
+
+
+-- kijken wie er de beurt aan is op basis van game_id
+
+SELECT 
+	g.game_id,
+    g.game_state,
+    MAX(tp1.turn_id) AS max_turn_p1_zet,
+    tp1.username_player1 AS max_turn_p1_username,
+    
+    MAX(tp2.turn_id) AS max_turn_p2_zet,
+    tp2.username_player2 AS max_turn_p2_username
+    
+FROM
+    game g
+        LEFT JOIN
+    turnplayer1 tp1 ON g.game_id = tp1.game_id
+        LEFT JOIN
+    turnplayer2 tp2 ON g.game_id = tp2.game_id
+    
+where (g.game_id = 502)
+GROUP BY g.game_id;
+
+
+
+-- een zet zetten
+
+INSERT INTO turnplayer1 (game_id, turn_id, username_player1, bonus, score, turnaction_type)
+VALUES ( @gameid, 1, 'Lidewij', 0, 96, 'play');
+
+INSERT INTO boardplayer1 (game_id, username, turn_id, letter_id, tile_x, tile_y)
+VALUES
+(@gameid, 'jagermeester', 1, 35, 6, 8),
+(@gameid, 'jagermeester', 1, 51, 7, 8),
+(@gameid, 'jagermeester', 1, 17, 8, 8),
+(@gameid, 'jagermeester', 1, 82, 9, 8);
+
+
+-- kijken of de score gelijk is tussen de spelers
+
+SELECT 
+	g.game_id,
+    g.game_state,
+    MAX(tp1.turn_id) AS max_turn_p1_zet,
+    tp1.username_player1 AS max_turn_p1_username,
+    tp1.score as max_turn_p1_score,
+    
+    MAX(tp2.turn_id) AS max_turn_p2_zet,
+    tp2.username_player2 AS max_turn_p2_username,
+    tp2.score as max_turn_p2_score
+    
+FROM
+    game g
+        LEFT JOIN
+    turnplayer1 tp1 ON g.game_id = tp1.game_id
+        LEFT JOIN
+    turnplayer2 tp2 ON g.game_id = tp2.game_id
+    
+where (g.game_id = 502)
+GROUP BY g.game_id;
+
+-- als de score gelijk is 5 punten toevoegen aan bonus
+UPDATE turnplayer1 
+SET bonus = 5 
+where turn_id = (select max(turn_id) from turnplayer1);
+
+UPDATE turnplayer2
+SET bonus = 5 
+where turn_id = (select max(turn_id) from turnplayer2);
 
