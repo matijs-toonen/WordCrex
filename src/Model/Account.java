067882fc4420505ba3a66c1609e195e053637530
Bader Ali <>
2018-12-13 23:07:53 +0100

    cleaned controllers

--- src/Model/Account.java
+++ src/Model/Account.java
@@ -10,6 +10,89 @@ import java.util.stream.Collectors;
 import Model.AccountRole.*;
 
 public class Account {
+	
+	
+	/*
+	 * PROPS
+	 */
+	private String _username;
+	private String _password;
+	private ArrayList<AccountRole> _roles = new ArrayList<AccountRole>();
+	
+	/*
+	 * COMNST
+	 */
+	public Account(String username) {
+		_username = username;
+	}
+	
+	public Account(String username, String password) {
+		this(username);
+		_password = password;
+	}
+	
+	public Account(String username, String password, String... roles) {
+		this(username, password);
+		addAllRoles(roles);
+	}
+	
+	public Account(ResultSet rs, ArrayList<String> columns) {
+		try {
+			_username = columns.contains("username") ? rs.getString("username") : null;
+			_password = columns.contains("password") ? rs.getString("password") : null;
+		} catch (SQLException e) {}
+	}
+	
+	public void addAllRoles(String... roles) {
+		for(var role : roles) {
+			_roles.add(role == null ? new PlayerRole() : getRole(role));	
+		}
+	}
+	
+	/*
+	 * Getters
+	 */
+	private AccountRole getRole(String role) {
+		switch(role.toLowerCase()) {
+		case "administrator":
+			return new AdministratorRole();
+		case "observer":
+			return new ObserverRole();
+		case "player":
+			return new PlayerRole();
+		case "moderator":
+			return new ModeratorRole();
+		default:
+			return null;
+		}
+	}
+
+	public String getUsername() {
+		return _username;
+	}
+  
+	public ArrayList<AccountRole> getRoles()
+	{
+		return _roles;
+	}
+	
+	public String getPassword() 
+	{
+		return _password;
+	}
+	
+	public static Optional<Account> getAccountByUsername(ArrayList<Account> accounts, String username){
+		return accounts.stream().filter(account -> account.getUsername().equals(username)).findFirst();
+	}
+	
+	public static List<Account> getAllAccountsByUsername(ArrayList<Account> accounts, String username){
+		return accounts.stream().filter(account -> account.getUsername().contains(username)).collect(Collectors.toList());
+	}
+	
+	
+	/*
+	 * Queries
+	 */
 	public static final String getAllAccounts() {
 		return ("select username, role from accountrole order by username");
 	}
@@ -40,77 +123,8 @@ public class Account {
 		return "SELECT * FROM accountrole";
 	}
 	
-	private String _username;
-	private String _password;
-	private ArrayList<AccountRole> _roles = new ArrayList<AccountRole>();
-	
 	public static final String updatePassword(String password, String username) {
 		return ("UPDATE account SET password = '" + password + "' WHERE username = '" + username + "'");
-	}
-	
-	public Account(String username) {
-		_username = username;
-	}
-	
-	public Account(String username, String password) {
-		this(username);
-		_password = password;
-	}
-	
-	public Account(String username, String password, String... roles) {
-		this(username, password);
-		addAllRoles(roles);
-	}
-	
-	public Account(ResultSet rs, ArrayList<String> columns) {
-		try {
-			_username = columns.contains("username") ? rs.getString("username") : null;
-			_password = columns.contains("password") ? rs.getString("password") : null;
-		} catch (SQLException e) {}
-	}
-	
-	public void addAllRoles(String... roles) {
-		for(var role : roles) {
-			_roles.add(role == null ? new PlayerRole() : getRole(role));	
-		}
-	}
-	
-	private AccountRole getRole(String role) {
-		switch(role.toLowerCase()) {
-		case "administrator":
-			return new AdministratorRole();
-		case "observer":
-			return new ObserverRole();
-		case "player":
-			return new PlayerRole();
-		case "moderator":
-			return new ModeratorRole();
-		default:
-			return null;
-		}
-	}
-
-	public String getUsername() {
-		return _username;
-	}
-
-  
-	public ArrayList<AccountRole> getRoles()
-	{
-		return _roles;
-	}
-	
-	public String getPassword() 
-	{
-		return _password;
-	}
-	
-	public static Optional<Account> getAccountByUsername(ArrayList<Account> accounts, String username){
-		return accounts.stream().filter(account -> account.getUsername().equals(username)).findFirst();
-	}
-	
-	public static List<Account> getAllAccountsByUsername(ArrayList<Account> accounts, String username){
-		return accounts.stream().filter(account -> account.getUsername().contains(username)).collect(Collectors.toList());
 	}
 	
 }
