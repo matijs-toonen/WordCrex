60e3a30e3bfa5f2682d9e1ef5401a6b47a8f1ee4
Xandor Janssen <xpjh.janssen@student.avans.nl>
2018-12-13 10:40:51 +0100

    turn query finished

--- queries/Bord.sql
+++ queries/Bord.sql
@@ -26,9 +26,10 @@ from handletter
 where game_id = 500);
 
 
--- kijken wie er de beurt aan is
 
+-- BORD
 
+-- kijken wie er de beurt aan is op basis van game_id
 
 SELECT 
 	g.game_id,
@@ -48,5 +49,63 @@ FROM
     
 where (g.game_id = 502)
 GROUP BY g.game_id;
+
+
+
+-- make playerturn and insert word
+
+INSERT INTO turnplayer1 (game_id, turn_id, username_player1, bonus, score, turnaction_type)
+VALUES ( @gameid, 1, 'Lidewij', 0, 96, 'play');
+
+INSERT INTO boardplayer1 (game_id, username, turn_id, letter_id, tile_x, tile_y)
+VALUES
+(@gameid, 'jagermeester', 1, 35, 6, 8),
+(@gameid, 'jagermeester', 1, 51, 7, 8),
+(@gameid, 'jagermeester', 1, 17, 8, 8),
+(@gameid, 'jagermeester', 1, 82, 9, 8);
+
+
+-- update score if score = gelijk
+
+
+SELECT 
+	g.game_id,
+    g.game_state,
+    MAX(tp1.turn_id) AS max_turn_p1_zet,
+    tp1.username_player1 AS max_turn_p1_username,
+    tp1.score as max_turn_p1_score,
+    
+    MAX(tp2.turn_id) AS max_turn_p2_zet,
+    tp2.username_player2 AS max_turn_p2_username,
+    tp2.score as max_turn_p2_score
+    
+FROM
+    game g
+        LEFT JOIN
+    turnplayer1 tp1 ON g.game_id = tp1.game_id
+        LEFT JOIN
+    turnplayer2 tp2 ON g.game_id = tp2.game_id
+    
+where (g.game_id = 502)
+GROUP BY g.game_id;
+
+-- als hij gelijk is 
+
+UPDATE turnplayer1 
+SET bonus = 5 
+where turn_id = (select max(turn_id) from turnplayer1);
+
+UPDATE turnplayer2
+SET bonus = 5 
+where turn_id = (select max(turn_id) from turnplayer2);
+
+
+
+
+
+
+
+
+
 
 
